ab7389b1a39654fb93bd5008e6f3276f2180f801
Sebastian Müller <seb.mueller.91@web.de>
2021-12-13 12:05:21 +0100

    Read in file

---
+++ Challenges/18/MaximumPathSumI/MaximumPathSumI/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumPathSumI
+{
+    internal class Triangle
+    {
+        private List<int[]> _entries;
+        public Triangle(int rows)
+        {
+            _entries = new List<int[]>();
+            for (int i = 0; i < rows; i++)
+            {
+                _entries.Add(new int[i+1]);
+            }
+        }
+
+        public int[] this[int row]
+        {
+            get => GetRow(row);
+            set => SetRow(row, value);
+        }
+
+        public void SetEntry(int row, int col, int value)
+        {
+            _entries[row][col] = value;
+        }
+
+        public void SetRow(int row, int[] values)
+        {
+            _entries[row] = values;
+        }
+
+        public int GetEntry(int row, int col)
+        {
+            return _entries[row][col];
+        }
+
+        public int[] GetRow(int row)
+        {
+            return _entries[row];
+        }
+    }
+}
---
+++ Challenges/18/MaximumPathSumI/MaximumPathSumI/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumPathSumI
+{
+    public static class Utils
+    {
+        internal static Triangle ReadTriangleFromFile(string path = @"..\..\..\..\..\triangle.txt")
+        {
+            var lines = System.IO.File.ReadAllLines(path);
+            var triangle = new Triangle(lines.Length);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    var parsedLine = lines[i].Split(' ');
+                    triangle[i][j] = Int32.Parse(parsedLine[j]);
+                }
+            }
+
+            return triangle;
+        }
+    }
+}
