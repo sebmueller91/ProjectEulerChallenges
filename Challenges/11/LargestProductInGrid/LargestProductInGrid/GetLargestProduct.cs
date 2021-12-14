311a39c971ba8e11cb3339ef319117dcbf200f39
Sebastian Müller <seb.mueller.91@web.de>
2021-12-14 08:53:19 +0100

    Challenge 11 started

---
+++ Challenges/11/LargestProductInGrid/LargestProductInGrid/GetLargestProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestProductInGrid
+{
+    internal class GetLargestProduct
+    {
+        internal static Dictionary<int, int> GetHighestProduct(int[,] grid)
+        {
+
+            return null;
+        }
+
+        private static Dictionary<int, int> GetLargestVerticalProduct()
+        {
+            var bestResult = new Dictionary<int, int>();
+            for (int i = 0; i < )
+        }
+        
+        private static long GetProduct(long n1, long n2, long n3, long n4)
+        {
+            return n1 * n2 * n3 * n4;
+        }
+    }
+}
---
+++ Challenges/11/LargestProductInGrid/LargestProductInGrid/Utils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestProductInGrid
+{
+    internal static class Utils
+    {
+        internal static int[,] ReadGridFromFile(string path = @"..\..\..\..\..\grid.txt")
+        {
+            var lines = System.IO.File.ReadAllLines(path);
+            int[,] grid = null;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    var parsedLine = lines[i].Split(' ');
+                    if (grid == null)
+                    {
+                        grid = new int[lines.Length, parsedLine.Length];
+                    }
+                    grid[i,j] = Int32.Parse(parsedLine[j]);
+                }
+            }
+
+            return grid;
+        }
+    }
+}
